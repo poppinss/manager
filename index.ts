1117b37b79f203a725a0fd23730cf84dab4a73b4
Harminder virk <virk.officials@gmail.com>
2019-05-20 12:14:22 +0530

    feat: implement manager

---
+++ index.ts
@@ -0,0 +1,10 @@
+/*
+* @poppinss/manager
+*
+* (c) Harminder Virk <virk@adonisjs.com>
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
+
+export { Manager } from './src/Manager'
